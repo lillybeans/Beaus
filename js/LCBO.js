20bd4bbcfd015ca82f0cd219f5342a614dc8b1f1
Lilly Tong <lilly.tong@mail.mcgill.ca>
2017-08-18 03:13:47 -0400

    added functionalities

--- js/LCBO.js
+++ js/LCBO.js
@@ -1,10 +1,21 @@
 var LCBO = new function() {
+    
+        this.BeausAllProducts=function(callback){
+            get("/products?q=Beaus&per_page=50", callback);
+        }
+        
+        this.BeausSeasonalProducts=function(query, callback){
+            get("/products?q=Beaus&where=is_seasonal&per_page=50"+query, callback);
+        }
+                
         this.getProduct = function(id, callback) {
           get('/products/' + id, callback);
         };
+    
         this.getStore = function(id, callback) {
           get('/stores/' + id, callback);
         };
+    
         function get(url, callback) {
           $.ajax({
             url: 'http://lcboapi.com' + url,
@@ -12,6 +23,7 @@ var LCBO = new function() {
             success: function(data) {
               if (200 == data.status) {
                 callback(data.result);
+                  console.log(data.result);
               } else {
                 alert('There was an error [' + data.status + ']: ' + data.message);
               }
@@ -19,18 +31,34 @@ var LCBO = new function() {
           });
         }
       };
+
       $(function() {
-        var product_id = 18;
-        LCBO.getProduct(product_id, function(product) {
-            
-          $('.product_title').text(product.name);
-            
-          $.each(product, function(key, value) {
-            if (key == 'name') return true;
-            $('#product_details').append(
-              "<br>" + key   +":"+ value +"<Br>"
-            );
+        
+        //On load: display all seasonal products
+        var query="&where_not=is_dead&order=released_on.desc";
+        
+          LCBO.BeausSeasonalProducts(query,function(products){
+              
+              $.each(products,function(i,product){
+                  
+                  //check if thumbnail exists, if not, replace with default image
+                  var product_img="<div class='product-img'><img class='thumb' src="+product.image_thumb_url+"></div>";
+                  if (product.image_thumb_url == null){
+                      product_img="<div class='product-img'><img class='not-found' src=img/beer-not-found.png></div>";
+                  }
+                  
+                  //fill in the information about this product
+                  $('.seasonal-list').append(
+                      "<li id="+product.id+">"
+                        +"<p class='product-name''>"+product.name+"</p>"+
+                        "<br>"+
+                        product_img+
+                      "<p class='volume'>"+product.volume_in_milliliters+" mL</p>"+
+                      "<p class='price'>$"+parseFloat(product.price_in_cents/100).toFixed(2)+"</p>"+
+                      "</li>"
+                  );
+		      });
+              
           });
-            
-        });
+          
       });
---
+++ js/main.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    $('.heading h1').addClass('animated fadeInDown');
+    $('.heading h2').addClass('animated fadeInUp');
+    
+    $('.seasonal-list').on('click', 'li', function(){
+
+        var product_id = $(this).attr("id"); //grab id from 'li'
+        
+        LCBO.getProduct(product_id,function(product){
+            
+              //title and image
+              //check if thumbnail exists, if not, replace with default image
+              var product_image="<div class='product-img clearfix'><img src="+product.image_url+"></div>";
+
+              if (product.image_url == null){
+                  product_image="<div class='product-img clearfix'><img class='not-found' src=img/beer-not-found.png></div>";
+              }
+            
+              var released_on = product.released_on;
+              if(product.released_on == null){
+                  released_on="Unknown";
+              }
+              
+              var serving_suggestion = product.serving_suggestion;
+              if(product.serving_suggestion==null){
+                  serving_suggestion="Just another really tasty beer!";
+              }
+
+              //fill in the information about this product
+              $('#product-details').html(
+                  "<div class='row'>"+
+                  "<h1>"+product.name+"</h1>"+
+                  "<h2>"+product.volume_in_milliliters+" mL</h2>"+
+                  product_image+
+                  "<table class='info clearfix'>"+
+                  "<tr><td>Price:</td><td> $"+(product.price_in_cents/100).toFixed(2)+"</td></tr>"+
+                  "<tr><td>Category: </td><td>"+product.primary_category+" - "+product.secondary_category+"</td></tr>"+
+                  "<tr><td>Origin: </td><td>"+product.origin+"</td></tr>"+
+                  "<tr><td>Producer: </td><td>"+product.producer_name+"</td></tr>"+
+                  "<tr><td>Released: </td><td>"+released_on+"</td></tr>"+
+                  "<tr><td>Suggestion: </td><td>"+serving_suggestion+"</td></tr></table>"+
+                  "</div>"
+
+              );
+              
+              $.each(product,function(key,value){
+                  
+       
+		      });
+              
+          });
+        
+        //smooth scroll
+        $('html, body').animate({
+            scrollTop: $("#product-details").offset().top
+        }, 1000);
+    });
+    
+});
